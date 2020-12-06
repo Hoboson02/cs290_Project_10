d52037fe6e1f38d83e7eab9fba3b7231398581a2
Ryan Earl <ryanleoearl@gmail.com>
2020-12-06 14:56:12 -0800

    Almost done with project

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,20 +1,40 @@
-
-const categories = [
-  { id: 3, name: 'Cruiser' },
-  { id: 2, name: 'Dual-Sport' },
-  { id: 1, name: 'Naked' },
-  { id: 4, name: 'Sport' }
-]
-
-const manufacturers = [
-  { id: 5, name: 'BMW' },
-  { id: 2, name: 'Honda' },
-  { id: 1, name: 'Indian' } ,
-  { id: 3, name: 'Triumph' },
-  { id: 4, name: 'Victory' }
-]
-
 document.addEventListener('DOMContentLoaded', bindNavBarClickHandlers)
+document.addEventListener('DOMContentLoaded', fetchAndDisplayFeaturedMotorcycle)
+
+function fetchAndDisplayFeaturedMotorcycle(event) { 
+  fetch('/motorcycles/featured').then( (response) => { 
+    if (response.ok) {
+      return response.json()
+    }
+  }).then( (motorcycles) => { 
+    let motorcyclesHeader = document.createElement('h2')
+    motorcyclesHeader.appendChild(document.createTextNode('motorcycles'))
+    let motorcyclesList = document.createElement('ul')
+    motorcyclesList.setAttribute('id', 'motorcycles')
+
+
+
+    motorcycles.forEach(category => {
+      let listItem = document.createElement('li')
+      let listItemHeader = document.createElement('h3')
+      let listItemLink = document.createElement('a')
+      listItemLink.setAttribute('href', '/category_' + category.id + '.html')
+      listItemLink.appendChild(document.createTextNode(category.name))
+      listItemHeader.appendChild(listItemLink)
+      listItem.appendChild(listItemHeader)
+      motorcyclesList.appendChild(listItem)
+    })
+    let main = document.querySelector('main')
+    while (main.hasChildNodes()) {
+      main.removeChild(main.lastChild);
+    }
+    main.appendChild(motorcyclesHeader)
+    main.appendChild(motorcyclesList)
+  
+  }) (featuredMotorcycle); {
+    console.log(featuredMotorcycle)
+  }
+}
 
 function bindNavBarClickHandlers(event) {
   let categoriesLink = document.getElementById('lnk-categories')
@@ -25,43 +45,61 @@ function bindNavBarClickHandlers(event) {
 
 function displayCategories(event) {
   event.preventDefault()
-  let categoriesHeader = document.createElement('h2')
-  categoriesHeader.appendChild(document.createTextNode('Categories'))
-  let categoriesList = document.createElement('ul')
-  categoriesList.setAttribute('id', 'categories')
-  categories.forEach(category => {
-    let listItem = document.createElement('li')
-    let listItemHeader = document.createElement('h3')
-    let listItemLink = document.createElement('a')
-    listItemLink.setAttribute('href', '/category_' + category.id + '.html')
-    listItemLink.appendChild(document.createTextNode(category.name))
-    listItemHeader.appendChild(listItemLink)
-    listItem.appendChild(listItemHeader)
-    categoriesList.appendChild(listItem)
+  fetch('/categories').then( (response) => { 
+    if (response.ok) { 
+      return response.json() 
+    }
+  }).then( (categories) => { 
+    let categoriesHeader = document.createElement('h2')
+    categoriesHeader.appendChild(document.createTextNode('Categories'))
+    let categoriesList = document.createElement('ul')
+    categoriesList.setAttribute('id', 'categories')
+
+
+
+    categories.forEach(category => {
+      let listItem = document.createElement('li')
+      let listItemHeader = document.createElement('h3')
+      let listItemLink = document.createElement('a')
+      listItemLink.setAttribute('href', '/category_' + category.id + '.html')
+      listItemLink.appendChild(document.createTextNode(category.name))
+      listItemHeader.appendChild(listItemLink)
+      listItem.appendChild(listItemHeader)
+      categoriesList.appendChild(listItem)
+    })
+    let main = document.querySelector('main')
+    while (main.hasChildNodes()) {
+      main.removeChild(main.lastChild);
+    }
+    main.appendChild(categoriesHeader)
+    main.appendChild(categoriesList)
   })
-  let main = document.querySelector('main')
-  while (main.hasChildNodes()) {
-    main.removeChild(main.lastChild);
-  }
-  main.appendChild(categoriesHeader)
-  main.appendChild(categoriesList)
+  
 }
 
 function displayManufacturers(event) {
   event.preventDefault()
-  let manufacturersHeader = document.createElement('h2')
-  manufacturersHeader.appendChild(document.createTextNode('Manufacturers'))
-  let manufacturersList = document.createElement('ul')
-  manufacturersList.setAttribute('id', 'manufacturers')
-  manufacturers.forEach(manufacturer => {
-    let listItem = document.createElement('li')
-    let listItemHeader = document.createElement('h3')
-    let listItemLink = document.createElement('a')
-    listItemLink.setAttribute('href', '/manufacturer_' + manufacturer.id + '.html')
-    listItemLink.appendChild(document.createTextNode(manufacturer.name))
-    listItemHeader.appendChild(listItemLink)
-    listItem.appendChild(listItemHeader)
-    manufacturersList.appendChild(listItem)
+  fetch('/manufacturers').then( (response) => { 
+    if (response.ok) { 
+      return response.json() 
+    }
+  }).then( (manufacturers) => { 
+    let manufacturersHeader = document.createElement('h2')
+    manufacturersHeader.appendChild(document.createTextNode('Manufacturers'))
+    let manufacturersList = document.createElement('ul')
+    manufacturersList.setAttribute('id', 'manufacturers')
+
+
+    
+    manufacturers.forEach(manufacturer => {
+      let listItem = document.createElement('li')
+      let listItemHeader = document.createElement('h3')
+      let listItemLink = document.createElement('a')
+      listItemLink.setAttribute('href', '/manufacturer_' + manufacturer.id + '.html')
+      listItemLink.appendChild(document.createTextNode(manufacturer.name))
+      listItemHeader.appendChild(listItemLink)
+      listItem.appendChild(listItemHeader)
+      manufacturersList.appendChild(listItem)
   })
   let main = document.querySelector('main')
   while (main.hasChildNodes()) {
@@ -69,4 +107,5 @@ function displayManufacturers(event) {
   }
   main.appendChild(manufacturersHeader)
   main.appendChild(manufacturersList)
+})
 }
--- server.js
+++ server.js
@@ -6,4 +6,11 @@ const { categories, manufacturers, motorcycles } = require('./data')
 
 app.use(express.static('public'))
 
+app.get('/categories', (req, res) => res.send(categories))
+app.get('/manufacturers', (req, res) => res.send(manufacturers))
+app.get('/motorcycles/featured', function(req, res) {
+motorcycles[Math.floor(Math.random() *Array.length)]; 
+    res.send(motorcycles)
+})
+
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
